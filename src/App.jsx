b760a9caa3bb6189e246c96b3e8046be45a9f1f2
Hendrik Nigul <hendrik@innovaatik.ee>
2024-05-24 22:29:45 +0300

    järgmine tööpäev

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
+Date.prototype.addDays = function(days) {
+  let date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+Date.prototype.isoString = function() {
+  let currentDate = new Date(this.valueOf());
+  const offset = currentDate.getTimezoneOffset();
+  const yourDate = new Date(currentDate.getTime() - (offset*60*1000));
+  return yourDate.toISOString().split('T')[0];
+}
+Date.prototype.displayString = function() {
+  let currentDate = new Date(this.valueOf());
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return currentDate.toLocaleString("et-EE", options);
+}
+
+const prefix = true ? 'https://cors-anywhere.herokuapp.com/' : '';
+const apiUrl = prefix + "https://xn--riigiphad-v9a.ee/en?output=json";
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [holidays, setHolidays] = useState(null);
+  const [okDate, setOkDate] = useState("");
+
+  useEffect(() => {
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(response => response.filter(x => ['1', '2'].includes(x.kind_id)).map(x => [x.date, x.title, x.kind_id]))
+        .then(response => setHolidays(response))
+        .catch(x => {
+          console.error('paha', x)
+        })
+      ;
+  }, []);
+
+  const generateDivs = (start) => {
+    if (!holidays) return [];
+    const newDivs = [];
+    let count = start;
+    while (count <= 100) {
+      const date = new Date().addDays(count);
+      const formatted = date.isoString();
+      const row = holidays.find(x => x[0] === formatted) || [];
+      const label = row[1] || [0, 6].includes(date.getDay()) && date.toLocaleString('et-EE', { weekday: "long"});
+      const local = date.displayString();
+      if (!label) {
+        if (okDate !== local) {
+          setOkDate(local);
+        }
+        break;
+      }
+      newDivs.push(<div key={count}>{label}: {local}</div>);
+      count +=1;
+    }
+    return newDivs;
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Oota {count} päeva
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="read-the-docs">
+        {generateDivs(count)}
+      </div>
+      <div><span className="read-the-docs">Järgmine tööpäev:</span> <span className="important">{okDate}</span></div>
     </>
   )
 }
